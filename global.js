d896f317fee7cf4bb59fd03d0cba7b704ffc8aff
ntd002 <ntd002@ucsd.edu>
2024-10-15 17:03:54 -0700

    Update global.js

--- global.js
+++ global.js
@@ -3,3 +3,14 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname,
+  );
+
+  if (currentLink) {
+    // or if (currentLink !== undefined)
+    currentLink.classList.add('current');
+  }
