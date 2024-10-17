55e2d9abe4d39cf0032b2fd650d53b5fc122669a
ntd002 <ntd002@ucsd.edu>
2024-10-16 17:09:13 -0700

    Update global.js

--- global.js
+++ global.js
@@ -57,8 +57,16 @@ for (let p of pages) {
 
 }
 
-//current tab is current
-
-
-  
-//making github profile target=blank
+//light/dark mode selector
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme:
+          <select>
+                <option value="automatic">--Automatic--</option>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+          </select>
+      </label>`,
+  );
