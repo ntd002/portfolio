b9b8a7918f7129f6520c8187feae14c12cbfb723
ntd002 <ntd002@ucsd.edu>
2024-10-16 17:28:03 -0700

    Update global.js

--- global.js
+++ global.js
@@ -64,7 +64,7 @@ document.body.insertAdjacentHTML(
       <label class="color-scheme">
           Theme:
           <select>
-                <option value="automatic">--Automatic--</option>
+                <option value="light dark">--Automatic--</option>
                 <option value="light">Light</option>
                 <option value="dark">Dark</option>
           </select>
@@ -75,4 +75,5 @@ let select = $$("select");
 
 select.addEventListener('input', function (event) {
     console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
 });
