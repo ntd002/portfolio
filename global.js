403d0ed1835364df5d40ee586b41b37c0e7695a1
ntd002 <ntd002@ucsd.edu>
2024-10-16 17:27:14 -0700

    Update global.js

--- global.js
+++ global.js
@@ -71,6 +71,8 @@ document.body.insertAdjacentHTML(
       </label>`,
   );
 
+let select = $$("select");
+
 select.addEventListener('input', function (event) {
     console.log('color scheme changed to', event.target.value);
 });
